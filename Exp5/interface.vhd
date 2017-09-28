256c8c60877aa4b7649fa3056b7873a5db50a7a6
joaoCasella <jpedrocaselladepaula@hotmail.com>
2017-09-28 18:21:43 -0300

    Created the interface module that conects the individual components

--- Exp5/interface.vhd
+++ Exp5/interface.vhd
@@ -5,6 +5,7 @@ use ieee.std_logic_1164.all;
 
 entity interface is
   port(clock                : in  std_logic;
+       reset                : in  std_logic;
        dadoRecebido         : in  std_logic_vector(6 downto 0);
        prontoRecepcao       : in  std_logic;
        paridadeOk           : in  std_logic;
@@ -16,7 +17,9 @@ entity interface is
        dadoTransmitido      : out std_logic_vector(6 downto 0);
        dadoRecepcao         : out std_logic_vector(6 downto 0);
        temDadoRecepcao      : out  std_logic;
-       transmissaoAndamento : out  std_logic);
+       transmissaoAndamento : out  std_logic;
+       estadoRecepcao       : out  std_logic_vector(2 downto 0);
+       estadoTransmissao    : out  std_logic_vector(2 downto 0));
 end interface;
 -- Criterio Adotado: quando for algo que liga a interface aos modulos
 -- de transmissao e recepcao, o nome esta como dadoTransmitido/Recebido
@@ -32,7 +35,8 @@ architecture interface of interface is
          recebeDado      : in   std_logic;
          zeraRegistrador : out  std_logic; -- zera registrador que armazena o sinal
          registraDado    : out  std_logic; -- controla registro do sinal recebido
-         temDadoRecepcao : out  std_logic);
+         temDadoRecepcao : out  std_logic;
+         estadoRecepcao  : out  std_logic_vector(2 downto 0));
   end component;
 
   component unidade_controle_transmissao is
@@ -43,42 +47,26 @@ architecture interface of interface is
          prontoTransmissao       : in   std_logic;
          partidaTransmissao      : out  std_logic;
          dadoTransmitido         : out  std_logic_vector(6 downto 0);
-         transmissaoAndamento    : out  std_logic);
+         transmissaoAndamento    : out  std_logic;
+         estadoTransmissao       : out  std_logic_vector(2 downto 0));
   end component;
 
-  component fluxo_dados_transmissao is
-    port(enableTransmissao: in  std_logic;
-         dadoSerial       : in  std_logic;
-         TD               : out std_logic);
+  component registrador is
+    port(clock            : in  std_logic;
+         reset            : in  std_logic;
+         enableRegistro   : in  std_logic;
+         dadoRecebido     : in  std_logic_vector(6 downto 0);
+         dadoRecepcao     : out std_logic_vector(6 downto 0));
   end component;
 
-  component fluxo_dados_recepcao is
-    port(enableRecepcao : in std_Logic;
-         RD             : in std_logic;
-         dadoRecebido   : out std_logic);
-  end component;
-
-signal estadoRecepcao : std_logic_vector(3 downto 0);
-signal estadoTransmissao : std_logic_vector(3 downto 0);
-signal DTRRecepcao : std_logic;
-signal DTRTransmissao : std_logic;
-signal enableTransmissao : std_logic;
-signal enableRecepcao : std_logic;
+signal zeraRegistrador : std_logic;
+signal registraDado    : std_logic;
 
 begin
 
-  uc_recepcao : unidade_controle_recepcao port map (clock, reset, liga, CD, RD, DTRRecepcao, enableRecepcao, estadoRecepcao);
-  uc_transmissao : unidade_controle_transmissao port map (clock, reset, liga, enviar, CTS, DTRTransmissao, RTS, enableTransmissao, estadoTransmissao);
+  uc_recepcao : unidade_controle_recepcao port map (clock, reset, prontoRecepcao, recebeDado, zeraRegistrador, registraDado, temDadoRecepcao, estadoRecepcao);
+  uc_transmissao : unidade_controle_transmissao port map (clock, reset, dadoTransmissao, transmiteDado, prontoTransmissao, partidaTransmissao, dadoTransmitido, transmissaoAndamento, estadoTransmissao);
 
-  fd_recepcao    : fluxo_dados_recepcao port map (enableRecepcao, RD, dadoRecebido);
-  fd_transmissao : fluxo_dados_transmissao port map (enableTransmissao, dadoSerial, TD);
-
-
-
-  estado_recepcao <= estadoRecepcao;
-  estado_transmissao <= estadoTransmissao;
-  DTR <= DTRRecepcao and DTRTransmissao;
-  envioOk <= enableTransmissao;
-  temDadoRecebido <= enableRecepcao;
+  fd_recepcao    : registrador port map (clock, reset or zeraRegistrador, registraDado, dadoRecebido, dadoRecepcao);
 
 end interface;
